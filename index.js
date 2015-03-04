c7136f49a98ce1544a4dee89b907d0cafc0299d9
asonnenschein <adrian.sonnenschein@gmail.com>
2015-03-03 19:47:29 -0700

    calculate bbox of raster

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var _ = require('lodash')
   , path = require('path')
   , program = require('commander')
   , queue = []
+  , util = require('util')
 ;
 
 program
@@ -45,40 +46,91 @@ function listRasters (callback) {
 
 function displayMetadata () {
 
-  var dataset;
-
   if (fs.statSync(program.path).isFile()) {
-    dataset = gdal.open(program.path);
-    console.log("number of bands: " + dataset.bands.count());
-    console.log("width: " + dataset.rasterSize.x);
-    console.log("height: " + dataset.rasterSize.y);
-    console.log("geotransform: " + dataset.geoTransform);
-    console.log("srs: " + (dataset.srs ? dataset.srs.toWKT() : 'null'));
-    console.log("metadata: %j", dataset.getMetadata());
-    console.log("");
-    console.log("band 1: ", dataset.bands.get(1));
+    metadata(program.path);
+  }
+  else {
+    listRasters(function (rasters) {
+      _.each(rasters, function (raster) {
+        raster = path.join(program.path, raster);
+        metadata(raster);
+      });
+    });
   }
 
+  function metadata (filePath) {
+
+    var cornerNames
+      , coordTransform
+      , corners
+      , driver
+      , driverMetadata
+      , ds
+      , geotransform
+      , size
+      , wgs84
+    ;
+
+    ds = gdal.open(filePath);
+
+    driver = ds.driver;
+    driverMetadata = driver.getMetadata();
+
+    if (driverMetadata['DCAP_RASTER'] !== 'YES') {
+      console.error('Source file is not a raster');
+      process.exit(1);
+    }
+
+    console.log('Driver: ' + driver.description);
+
+    size = ds.rasterSize;
+    console.log('Size is ' + size.x + ', ' + size.y);
+
+    console.log('Coordinate System is: ');
+    console.log(ds.srs.toPrettyWKT());
+
+    geotransform = ds.geoTransform;
+    console.log('Origin = (' + geotransform[0] + ', ' + geotransform[3] + ')');
+    console.log('Pixel Size = (' + geotransform[1] + ', ' + geotransform[5] + ')');
+    console.log('GeoTransform =');
+    console.log(geotransform);
+
+    corners = {
+      'Upper Left  ' : {x: 0, y: 0},
+      'Upper Right ' : {x: size.x, y: 0},
+      'Bottom Right' : {x: size.x, y: size.y},
+      'Bottom Left ' : {x: 0, y: size.y},
+      'Center      ' : {x: size.x/2, y: size.y/2}
+    };
+
+    wgs84 = gdal.SpatialReference.fromEPSG(4326);
+    coordTransform = new gdal.CoordinateTransformation(ds.srs, wgs84);
+
+    console.log("Corner Coordinates:")
+    cornerNames = Object.keys(corners);
+    cornerNames.forEach(function(cornerName) {
+      var corner
+        , description
+        , ptOrig
+        , ptWgs84
+      ;
+
+      corner = corners[cornerName]
+      ptOrig = {
+        x: geotransform[0] + corner.x * geotransform[1] + corner.y * geotransform[2],
+        y: geotransform[3] + corner.x * geotransform[4] + corner.y * geotransform[5]
+      };
+      ptWgs84 = coordTransform.transformPoint(ptOrig);
+      description = util.format('%s (%d, %d) (%s, %s)',
+        cornerName,
+        Math.floor(ptOrig.x * 100) / 100,
+        Math.floor(ptOrig.y * 100) / 100,
+        gdal.decToDMS(ptWgs84.x, 'Long'),
+        gdal.decToDMS(ptWgs84.y, 'Lat')
+      );
+      console.log(description);
+    });
+
+  };
+
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
